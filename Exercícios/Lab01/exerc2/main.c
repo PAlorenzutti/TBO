1cbd4da2c52f68a351ced253cc341e4c8f8c5664
PAlorenzutti <pedro.lorenzutti@gmail.com>
2025-05-08 19:07:06 -0300

    Adicionado exerc2

---
+++ Exercícios/Lab01/exerc2/main.c
@@ -0,0 +1,33 @@
+#include "vector.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    Vector *pessoas = vector_construct();
+
+    int N, M;
+    scanf("%d %d\n", &N, &M);
+
+    //constrói o vetor de pessoas;
+    for(int i = 0; i < N; i++){
+        int *pessoa = (int*)malloc(sizeof(int));
+        *pessoa = i;
+        vector_push_back(pessoas, pessoa);
+    }
+
+    int index_atual = 0;
+
+    while(vector_size(pessoas) > 1){
+        index_atual = (index_atual + M - 1) % vector_size(pessoas);
+
+        int *pessoa_removida = vector_remove(pessoas, index_atual);
+
+        printf("Pessoa %d foi removida da roda\n", *pessoa_removida + 1);
+    }
+
+    int *lider = vector_pop_back(pessoas);
+    printf("Pessoa %d é a líder\n", *lider + 1);
+
+    return 0;
+}
